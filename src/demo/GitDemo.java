d1949449dfbc27e62c49a08ca54340edbfab7b5a
vinayak mane <vinayak mane>
2016-11-11 10:14:05 +0530

    gsfhslf[lshcksncl xcchxkjcv'al

--- src/demo/GitDemo.java
+++ src/demo/GitDemo.java
@@ -4,12 +4,9 @@ public class GitDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi EveryONe");
-
-System.out.println("Hi EveryONe");
-System.out.println("Hi EveryONe");
-System.out.println("Hi EveryONe");
-System.out.println("Hi EveryONe");		
+		System.out.println("Hi EveryONe");	
+	System.out.println("Hi EveryONe");
+	
 	}
 
 }
